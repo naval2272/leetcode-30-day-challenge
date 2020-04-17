2252b8f493274038724d77577872410bce6e2bbc
naval pandey <naval.pandey@morningstar.com>
2020-04-16 21:48:48 -0400

    leetcode-30-day-challenge

---
+++ src/main/java/day1.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class day1 {
+  public int singleNumber1(int[] nums) {
+    int sum = 0;
+    for(int i : nums) {
+      sum = sum ^ i;
+    }
+    return sum;
+  }
+
+  public int singleNumber2(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap();
+    for(int i : nums) {
+        if(!map.containsKey(i)) {
+          map.put(i, 1);
+    } else {
+      map.put(i, map.get(i) + 1);
+    }
+    }
+
+    for(Integer key : map.keySet()) {
+      if(map.get(key) == 1) return key;
+    }
+
+    return -1;
+  }
+}
